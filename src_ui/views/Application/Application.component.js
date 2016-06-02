6111fea85415d5b4ff66a0b93d32bf719c696509
M.Jackson <mwj8410@gmail.com>
2016-06-02 21:20:30 +0000

    chore(application): remove residual code from seed project

--- src_ui/views/Application/Application.component.js
+++ src_ui/views/Application/Application.component.js
@@ -2,17 +2,7 @@
 angular.module('app').component('application', { // Rename
   bindings: {},
   controller: function () {
-    this.activePage = 'titleScreen';
-    var scope = this;
 
-    var navigateTo = function (event) {
-      if (!event.navigationTarget || event.navigationTarget === '') {
-        return;
-      }
-      scope.activePage = event.navigationTarget;
-    };
-
-    document.addEventListener('application-navigation', navigateTo);
   },
   templateUrl: 'Application.template.html',
   transclude: {}
